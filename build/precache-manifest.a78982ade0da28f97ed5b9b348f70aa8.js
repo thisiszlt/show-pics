687933550488c22d471d76ce77c25774605540b5
zhuangliuting <18868104993@163.com>
2020-06-01 13:25:30 +0800

    update

---
+++ build/precache-manifest.a78982ade0da28f97ed5b9b348f70aa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93289c5f1516d62e81f1fce40ffb1eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6c97cfe8c20f645dde",
+    "url": "/static/css/2.c34640d7.chunk.css"
+  },
+  {
+    "revision": "a5d0702b48f09f2cca36",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc6c97cfe8c20f645dde",
+    "url": "/static/js/2.161a2975.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/static/js/2.161a2975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d0702b48f09f2cca36",
+    "url": "/static/js/main.f6163a8a.chunk.js"
+  },
+  {
+    "revision": "e01008046b569c3c99bc",
+    "url": "/static/js/runtime-main.3781ce9b.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Modal} from "antd-mobile";
+import 'antd-mobile/dist/antd-mobile.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const picData = Array.from(new Array(4)).map((val, index) => ({
+  picture: index + ".PNG"
+}));
+const winHeight=window.innerHeight;
+// const winWidth = window.innerWidth;
+
+function closest(el, selector) {
+  const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+  while (el) {
+    if (matchesSelector.call(el, selector)) {
+      return el;
+    }
+    el = el.parentElement;
+  }
+  return null;
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pic0: false,
+      pic1: false,
+      pic2: false,
+      pic3: false
+    };
+  }
+
+  showModal = key => (e) => {
+    e.preventDefault(); // 修复 Android 上点击穿透
+    console.log(key);
+    this.setState({
+      [key]: true,
+    });
+  }
+
+  onClose = key => () => {
+    this.setState({
+      [key]: false,
+    });
+  }
+  onWrapTouchStart = (e) => {
+    // fix touch to scroll background page on iOS
+    if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+      return;
+    }
+    const pNode = closest(e.target, '.am-modal-content');
+    if (!pNode) {
+      e.preventDefault();
+    }
+  }
+
+  render(){
+    const PicDetail = Array.from(new Array(4)).map((val, index) => {
+      var pic = `pic${index}`;
+      return (
+      <Modal
+        visible={this.state[pic]}
+        transparent
+        onClose={this.onClose(pic)}
+        popup
+        wrapProps={{ onTouchStart: this.onWrapTouchStart }}
+        key={pic}
+      >
+        <div style={{ position:"relative", height: winHeight-140, overflow: 'scroll' }}>
+          <img src={`${index}.PNG`} alt="" style={{height:"100%",objectFit:"cover"}}></img>
+        </div>
+      </Modal>);
+    });
+    
+    return (
+      <div style={{position: "absolute",transform: "translate(-50%,50%)",bottom: "50%",left:"50%"}}>
+        <Grid data={picData}
+        columnNum={2}
+        square={false}
+        hasLine={false}
+        renderItem={(dataItem, index) => (
+          <div style={{ padding: 0 }} onClick={this.showModal(`pic${index}`)}>
+            <img src={dataItem.picture} style={{height:winHeight/2 - 100 }} alt="" />
+          </div>
+        )}
+        itemStyle={{padding:0}}
+        // onClick={(dataItem, index)=>{
+        //   this.showModal("pic"+index)();
+        // }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </Grid>
+
+        {PicDetail}
+
+      </div>
+  
+    );
+  }
+
 }
 
 export default App;
